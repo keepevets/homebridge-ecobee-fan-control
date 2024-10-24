d7a4f96b5b61086435627f64b3feb76da82cbf12
Spencer <sbsargent@pitt.edu>
2024-10-24 11:15:10 -0400

    Add sleep mode support using SecuritySystem service

--- src/awaySwitchAccessory.ts
+++ src/awaySwitchAccessory.ts
@@ -1,9 +1,6 @@
 import { Service, PlatformAccessory, CharacteristicValue, CharacteristicSetCallback, CharacteristicGetCallback } from 'homebridge';
-
 import axios from 'axios';
-
 import { EcobeeAPIPlatform } from './platform';
-
 import { AuthTokenManager } from './auth-token-refresh';
 
 /**
@@ -14,58 +11,92 @@ import { AuthTokenManager } from './auth-token-refresh';
 export class AwaySwitchAccessory {
 	private service: Service;
 
+	// Define constants for climate states
+	private readonly CLIMATE_HOME = 'home';
+	private readonly CLIMATE_AWAY = 'away';
+	private readonly CLIMATE_SLEEP = 'sleep';
+
 	constructor(
 		private readonly platform: EcobeeAPIPlatform,
 		private readonly accessory: PlatformAccessory,
 	) {
-
 		// set accessory information
 		this.accessory.getService(this.platform.Service.AccessoryInformation)!
 			.setCharacteristic(this.platform.Characteristic.Manufacturer, 'Ecobee')
-			.setCharacteristic(this.platform.Characteristic.Model, 'Away')
+			.setCharacteristic(this.platform.Characteristic.Model, 'Climate Controller')
 			.setCharacteristic(this.platform.Characteristic.SerialNumber, 'ECOBEEAWAY1');
 
-		// get the service if it exists, otherwise create a new service
-		// you can create multiple services for each accessory
-		this.service = this.accessory.getService(this.platform.Service.Switch) || this.accessory.addService(this.platform.Service.Switch);
+		// Use SecuritySystem service instead of Switch
+		this.service = this.accessory.getService(this.platform.Service.SecuritySystem) 
+			|| this.accessory.addService(this.platform.Service.SecuritySystem);
 
-		// To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-		// when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-		// this.accessory.getService('NAME') ?? this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE');
-
-		// set the service name, this is what is displayed as the default name on the Home app
-		// in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+		// Set the service name
 		this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.displayName);
 
-		// each service must implement at-minimum the "required characteristics" for the given service type
-		// see https://developers.homebridge.io/#/service/Lightbulb
+		// Register handlers for the SecuritySystemTargetState Characteristic
+		this.service.getCharacteristic(this.platform.Characteristic.SecuritySystemTargetState)
+			.setProps({
+				validValues: [
+					this.platform.Characteristic.SecuritySystemTargetState.STAY_ARM,  // Home
+					this.platform.Characteristic.SecuritySystemTargetState.AWAY_ARM,  // Away
+					this.platform.Characteristic.SecuritySystemTargetState.NIGHT_ARM, // Sleep
+				],
+			})
+			.on('set', this.setTargetState.bind(this))
+			.on('get', this.getTargetState.bind(this));
 
-		// register handlers for the On/Off Characteristic
-		this.service.getCharacteristic(this.platform.Characteristic.On)
-			.on('set', this.setOn.bind(this))                // SET - bind to the `setOn` method below
-			.on('get', this.getOn.bind(this));               // GET - bind to the `getOn` method below
+		// Register handlers for the SecuritySystemCurrentState Characteristic
+		this.service.getCharacteristic(this.platform.Characteristic.SecuritySystemCurrentState)
+			.on('get', this.getCurrentState.bind(this));
 
-		// This can be used to poll the server and update the status of our switch without waiting
-		// for the getter to be called
+		// Poll for updates
 		setInterval(async () => {
 			const apiStatus = await this.checkStatusFromAPI();
-			// implement your own code to check if the device is on
-			const isOn = apiStatus === 'away';
-
-			// push the new value to HomeKit
-			this.service.updateCharacteristic(this.platform.Characteristic.On, isOn);
-
-			this.platform.log.debug('Pushed updated current state to HomeKit: ', apiStatus);
+			const currentState = this.mapClimateToSecurityState(apiStatus);
+			this.service.updateCharacteristic(this.platform.Characteristic.SecuritySystemCurrentState, currentState);
+			this.service.updateCharacteristic(this.platform.Characteristic.SecuritySystemTargetState, currentState);
+			this.platform.log.debug('Pushed updated current state to HomeKit:', apiStatus);
 		}, 30 * 60 * 1000);
 	}
 
 	/**
-	 * Handle "SET" requests from HomeKit
-	 * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
+	 * Maps climate mode to HomeKit security system state
 	 */
-	async setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+	private mapClimateToSecurityState(climate: string): number {
+		switch (climate) {
+		case this.CLIMATE_AWAY:
+			return this.platform.Characteristic.SecuritySystemCurrentState.AWAY_ARM;
+		case this.CLIMATE_SLEEP:
+			return this.platform.Characteristic.SecuritySystemCurrentState.NIGHT_ARM;
+		case this.CLIMATE_HOME:
+		default:
+			return this.platform.Characteristic.SecuritySystemCurrentState.STAY_ARM;
+		}
+	}
+
+	/**
+	 * Maps HomeKit security system state to climate mode
+	 */
+	private mapSecurityToClimate(state: number): string {
+		switch (state) {
+		case this.platform.Characteristic.SecuritySystemTargetState.AWAY_ARM:
+			return this.CLIMATE_AWAY;
+		case this.platform.Characteristic.SecuritySystemTargetState.NIGHT_ARM:
+			return this.CLIMATE_SLEEP;
+		case this.platform.Characteristic.SecuritySystemTargetState.STAY_ARM:
+		default:
+			return this.CLIMATE_HOME;
+		}
+	}
+
+	/**
+	 * Handle SET requests from HomeKit
+	 */
+	async setTargetState(value: CharacteristicValue, callback: CharacteristicSetCallback) {
 		try {
-			const targetValue = value as boolean;
+			const targetState = value as number;
+			const climateRef = this.mapSecurityToClimate(targetState);
+			
 			const needsRefresh = AuthTokenManager.getInstance().isExpired();
 			if (needsRefresh) {
 				await AuthTokenManager.getInstance().renewAuthToken();
@@ -74,29 +105,8 @@ export class AwaySwitchAccessory {
 			const selectionMatch = this.platform.config.thermostatSerialNumbers || '';
 			const selectionType = selectionMatch ? 'thermostats' : 'registered';
 
-			if (targetValue) {
-				// away
-				const awayBody = {
-					'selection': {
-						'selectionType': selectionType,
-						'selectionMatch': selectionMatch,
-					},
-					'functions': [
-						{
-							'type': 'setHold',
-							'params': {
-								'holdType': 'indefinite',
-								'holdClimateRef': 'away',
-							},
-						},
-					],
-				};
-				const awaySetRequest = await axios.post('https://api.ecobee.com/1/thermostat?format=json', awayBody, {headers: {'Authorization': 'Bearer ' + authToken}});
-				const awayData = awaySetRequest.data;
-
-				this.platform.log.info(`Set Ecobee to away with result: ${JSON.stringify(awayData)}`);
-			} else {
-				// home
+			if (climateRef === this.CLIMATE_HOME) {
+				// Resume program for home mode
 				const homeBody = {
 					'selection': {
 						'selectionType': selectionType,
@@ -111,73 +121,90 @@ export class AwaySwitchAccessory {
 						},
 					],
 				};
-				const homeSetRequest = await axios.post('https://api.ecobee.com/1/thermostat?format=json', homeBody, {headers: {'Authorization': 'Bearer ' + authToken}});
-				const homeData = homeSetRequest.data;
-
-				this.platform.log.info(`Set Ecobee to home with result: ${JSON.stringify(homeData)}`);
+				const homeSetRequest = await axios.post('https://api.ecobee.com/1/thermostat?format=json', 
+					homeBody, 
+					{headers: {'Authorization': 'Bearer ' + authToken}},
+				);
+				this.platform.log.info(`Set Ecobee to home with result: ${JSON.stringify(homeSetRequest.data)}`);
+			} else {
+				// Set hold for away or sleep mode
+				const setHoldBody = {
+					'selection': {
+						'selectionType': selectionType,
+						'selectionMatch': selectionMatch,
+					},
+					'functions': [
+						{
+							'type': 'setHold',
+							'params': {
+								'holdType': 'indefinite',
+								'holdClimateRef': climateRef,
+							},
+						},
+					],
+				};
+				const setHoldRequest = await axios.post('https://api.ecobee.com/1/thermostat?format=json', 
+					setHoldBody, 
+					{headers: {'Authorization': 'Bearer ' + authToken}},
+				);
+				this.platform.log.info(`Set Ecobee to ${climateRef} with result: ${JSON.stringify(setHoldRequest.data)}`);
 			}
 
-			this.platform.log.debug('Set Characteristic On ->', value);
-
+			this.platform.log.debug('Set Target State ->', value);
 			callback(null);
-		} catch(error){
+		} catch (error) {
 			callback(error as Error);
 		}
 	}
 
 	/**
-	 * Handle the "GET" requests from HomeKit
-	 * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-	 *
-	 * GET requests should return as fast as possbile. A long delay here will result in
-	 * HomeKit being unresponsive and a bad user experience in general.
-	 *
-	 * If your device takes time to respond you should update the status of your device
-	 * asynchronously instead using the `updateCharacteristic` method instead.
-
-	 * @example
-	 * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
+	 * Handle GET requests for target state
 	 */
-	async getOn(callback: CharacteristicGetCallback) {
+	async getTargetState(callback: CharacteristicGetCallback) {
 		const apiStatus = await this.checkStatusFromAPI();
-		// implement your own code to check if the device is on
-		const isOn = apiStatus === 'away';
-
-		this.platform.log.debug('Get Characteristic On ->', isOn);
-
-		// you must call the callback function
-		// the first argument should be null if there were no errors
-		// the second argument should be the value to return
-		callback(null, isOn);
+		const state = this.mapClimateToSecurityState(apiStatus);
+		this.platform.log.debug('Get Target State ->', state);
+		callback(null, state);
 	}
 
-	private async checkStatusFromAPI() {
+	/**
+	 * Handle GET requests for current state
+	 */
+	async getCurrentState(callback: CharacteristicGetCallback) {
+		const apiStatus = await this.checkStatusFromAPI();
+		const state = this.mapClimateToSecurityState(apiStatus);
+		this.platform.log.debug('Get Current State ->', state);
+		callback(null, state);
+	}
+
+	/**
+	 * Check the current climate status from the Ecobee API
+	 */
+	private async checkStatusFromAPI(): Promise<string> {
 		const needsRefresh = AuthTokenManager.getInstance().isExpired();
 		if (needsRefresh) {
 			await AuthTokenManager.getInstance().renewAuthToken();
 		}
 		const authToken = AuthTokenManager.getInstance().authToken;
 
-		const queryRequest = await axios.get('https://api.ecobee.com/1/thermostat?format=json&body={"selection":{"selectionType":"registered","selectionMatch":"","includeEvents":true}}', {headers: {'Authorization': 'Bearer ' + authToken}});
+		const queryRequest = await axios.get(
+			'https://api.ecobee.com/1/thermostat?format=json&body={"selection":{"selectionType":"registered","selectionMatch":"","includeEvents":true}}',
+			{headers: {'Authorization': 'Bearer ' + authToken}},
+		);
 		const queryData = queryRequest.data;
 
 		if (!queryData || !queryData.thermostatList) {
 			this.platform.log.error('Unexpected query data: ' + JSON.stringify(queryData));
-
-			// Ideally we might want to put the accessory into a "Not Responding" state but for now we'll just claim normal operation
-			return 'home';
+			return this.CLIMATE_HOME;
 		}
 			
 		const events = queryData.thermostatList[0].events;
 
-		if(events.length > 0){
+		if (events.length > 0) {
 			const mostRecentEvent = events[0];
-			const awayMode = mostRecentEvent.holdClimateRef === 'away';
-			return (awayMode) ? 'away' : 'home';
+			return mostRecentEvent.holdClimateRef || this.CLIMATE_HOME;
 		} else {
-			// No events means normal program, or "home"
-			return 'home';
+			return this.CLIMATE_HOME;
 		}
 	}
-
 }
