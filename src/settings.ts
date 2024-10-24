fd46c0410b9a4eca386af22ce7c022f57b876a1c
Spencer <sbsargent@pitt.edu>
2024-10-24 13:05:38 -0400

    updated PLATFORM_NAME and PLUGIN_NAME to reflect the "Ecobee Status" fork

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'EcobeeAway';
+export const PLATFORM_NAME = 'EcobeeStatus';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-ecobee-away';
+export const PLUGIN_NAME = 'homebridge-ecobee-status';
